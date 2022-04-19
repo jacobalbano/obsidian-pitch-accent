180b5c805b758d4cf61ff605940077f2c760c6c4
Jacob Albano <jacob.albano@gmail.com>
2022-04-19 13:50:06 -0400

    First commit

--- main.ts
+++ main.ts
@@ -1,137 +1,54 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, MarkdownPostProcessor, MarkdownPostProcessorContext } from 'obsidian'
 
-// Remember to rename these classes and interfaces!
+// Regular Expression for expression{LHLL} format
+const REGEXP = /([^\s<>]+){([hlHL]+)}/g
 
-interface MyPluginSettings {
-	mySetting: string;
+// Main Tags to search for Furigana Syntax
+const TAGS = 'p, h1, h2, h3, h4, h5, h6, ol, ul, table'
+
+const convertPitch = (element: Text): Node => {
+	const matches = Array.from(element.textContent.matchAll(REGEXP))
+	for (const [_, expression, pitch] of matches) {
+		if (expression.length >= pitch.length) {
+			let target_str = expression.slice(-pitch.length);
+			let high_low = pitch.toLowerCase();
+			let resultEl = document.createElement('span');
+			[...high_low].forEach((item, index, array) => {
+				const cls = (index === 0 || item === array[index - 1])
+					? `tone-${item}`
+					: `tone-${item}-change`;
+					
+				resultEl.createEl('span', { cls, text: target_str[index] });
+			});
+
+			return resultEl;
+		}
+	}
+	return element
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class MarkdownPitch extends Plugin {
 	async onload() {
-		await this.loadSettings();
+		this.registerMarkdownPostProcessor((el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
+			const blockToReplace = el.querySelectorAll(TAGS)
+			if (blockToReplace.length === 0) return;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+			function replace(node: Node) {
+				const childrenToReplace: Text[] = []
+				node.childNodes.forEach(child => {
+					if (child.nodeType === 3) {
+						// Nodes of Type 3 are TextElements
+						childrenToReplace.push(child as Text);
+					} else if (child.hasChildNodes() && child.nodeName !== 'CODE' && child.nodeName !== 'RUBY') {
+						// Ignore content in Code Blocks
+						replace(child);
 					}
+				})
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				childrenToReplace.forEach((child) => child.replaceWith(convertPitch(child)));
 			}
-		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			blockToReplace.forEach(block => replace(block));
+		})
 	}
 }
